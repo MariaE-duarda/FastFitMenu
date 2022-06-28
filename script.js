935fb91057e7ae88ffa0bb6a98af10e954bad290
Maria Eduarda de Araujo Freire <eduardaa.araujof@gmail.com>
2022-06-28 18:49:03 -0300

    Alterações necessárias (experiência do usuário).

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const menu = [
         id: 1,
         title: "Mousse de Maracujá",
         category: "sobremesas",
-        price: 15.99,
+        price: 8,
         img: "./images/mousseMaracuja.jpg",
         desc: "Leite vegetal, leite de coco, xilitol, poupa de maracujá.",
     },
@@ -16,7 +16,7 @@ const menu = [
         id: 2,
         title: "Pudim Fit",
         category: "sobremesas",
-        price: 16.99,
+        price: 13,
         img: "./images/pudimFit.webp",
         desc: "Leite vegetal, leite de soja, ovo orgânico.",
     },
@@ -24,7 +24,7 @@ const menu = [
         id: 3,
         title: "Sufle de banana",
         category: "sobremesas",
-        price: 17.99,
+        price: 11,
         img: "./images/sufleBanana.jpg",
         desc: "Leite vegetal, canela, claras em neve.",
     },
@@ -32,7 +32,7 @@ const menu = [
         id: 4,
         title: "Petit gâteau fit",
         category: "sobremesas",
-        price: 26.99,
+        price: 15,
         img: "./images/PetitFit.jpg",
         desc: "Farinha de amêndoas, flavo de baunilha, açúcar demerara.",
     },
@@ -40,7 +40,7 @@ const menu = [
         id: 5,
         title: "Crème Brûlée",
         category: "sobremesas",
-        price: 26.99,
+        price: 10,
         img: "./images/cremeBrulee.jpg",
         desc: "Gema de ovos, leite de soja, essência de baunilha.",
     },
@@ -48,7 +48,7 @@ const menu = [
         id: 6,
         title: "Bolo de Chuva fit",
         category: "sobremesas",
-        price: 26.99,
+        price: 5,
         img: "./images/boloChuva.jpg",
         desc: "Leite de amêndoas, farinha sem glúten, cacau em pó.",
     },
@@ -56,7 +56,7 @@ const menu = [
         id: 7,
         title: "Mousse de limão",
         category: "sobremesas",
-        price: 26.99,
+        price: 6,
         img: "./images/mousseLimao.jpg",
         desc: "Leite vegetal, limão siciliano, flor de sal.",
     },
@@ -64,7 +64,7 @@ const menu = [
         id: 8,
         title: "Sanduíche funcional",
         category: "pratos",
-        price: 26.99,
+        price: 15,
         img: "./images/sanduicheFuncional.ashx",
         desc: "Pão integral, queijo vegetal, alface, rúcula.",
     },
@@ -72,7 +72,7 @@ const menu = [
         id: 9,
         title: "Empadão Fit",
         category: "pratos",
-        price: 26.99,
+        price: 30,
         img: "./images/empadaFit.webp",
         desc: "Creme de frango, farinha sem glúten, requeijão vegano, manteiga orgânica.",
     },
@@ -80,7 +80,7 @@ const menu = [
         id: 10,
         title: "Omelete de carne",
         category: "pratos",
-        price: 26.99,
+        price: 18,
         img: "./images/omeleteCarne.jpg",
         desc: "Ovos orgânicos, leite de soja, mistura de tempeiros.",
     },
@@ -88,7 +88,7 @@ const menu = [
         id: 11,
         title: "Panqueca de frango",
         category: "pratos",
-        price: 26.99,
+        price: 12,
         img: "./images/panquecaFrango.jpg",
         desc: "Leite vegetal, frango desfiado, milho-verde.",
     },
@@ -96,7 +96,7 @@ const menu = [
         id: 12,
         title: "Pizza de frango",
         category: "pizzas",
-        price: 26.99,
+        price: 45,
         img: "./images/pizzaFrango.jpg",
         desc: "Frango desfiado, queijo vegetal, fermento biológico.",
     },
@@ -104,15 +104,15 @@ const menu = [
         id: 13,
         title: "Hambúguer Fit",
         category: "pratos",
-        price: 26.99,
+        price: 15,
         img: "./images/hambuguerFit.webp",
         desc: "Porção sem glúten, hambúguer de carne, mistura de tempeiros.",
     },
     {
         id: 14,
         title: "Batata Assada",
-        category: "pratos",
-        price: 26.99,
+        category: "acompanhamentos",
+        price: 20,
         img: "./images/batataAssada.png",
         desc: "Batata, azeite, mistura de tempeiros, sal de rosa.",
     },
@@ -120,9 +120,89 @@ const menu = [
         id: 15,
         title: "Croissant",
         category: "pratos",
-        price: 26.99,
+        price: 7,
         img: "./images/croissant.webp",
         desc: "Pão sem glúten, azeitona fresca, peito de peru.",
+    },
+    {
+        id: 16,
+        title: "Pizza de peito de peru",
+        category: "pizzas",
+        price: 40,
+        img: "./images/pizzaPeru.jpg",
+        desc: "",
+    },
+    {
+        id: 17,
+        title: "Pizza de Brócolis",
+        category: "pizzas",
+        price: 42,
+        img: "./images/pizzaBrocolis.webp",
+        desc: "",
+    },
+    {
+        id: 18,
+        title: "Pizza de Salmão",
+        category: "pizzas",
+        price: 65,
+        img: "./images/pizzaSalmao.webp",
+        desc: "",
+    },
+    {
+        id: 19,
+        title: "Pizza de Espinafre",
+        category: "pizzas",
+        price: 40,
+        img: "./images/pizzaEspinafre.jpg",
+        desc: "",
+    },
+    {
+        id: 20,
+        title: "Batata Cremosa",
+        category: "acompanhamentos",
+        price: 25,
+        img: "./images/batataCremosa.jpg",
+        desc: "",
+    },
+    {
+        id: 21,
+        title: "Purê de abobóra",
+        category: "acompanhamentos",
+        price: 15,
+        img: "./images/pureAbobora.ashx",
+        desc: "",
+    },
+    {
+        id: 22,
+        title: "Jardineira de Legumes",
+        category: "acompanhamentos",
+        price: 18,
+        img: "./images/jardineiraLegume.jpg",
+        desc: "",
+    },
+    {
+        id: 23,
+        title: "Caponata de Berinjela",
+        category: "acompanhamentos",
+        price: 25,
+        img: "./images/caponata.jpg",
+        desc: "",
+    },
+    {
+        id: 24,
+        title: "Couve flor gratinada",
+        category: "acompanhamentos",
+        price: 20,
+        img: "./images/couveFlorGratinada.webp",
+        desc: "",
+    },
+    {
+        id: 25,
+        title: "Pirão de Banana",
+        category: "acompanhamentos",
+        price: 15,
+        img: "./images/piraoBanana.jpg",
+        desc: "",
     }
 ]
 
@@ -139,41 +219,41 @@ window.addEventListener("DOMContentLoaded", function () {
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const category = e.currentTarget.dataset.id
-        const menuCategory = menu.filter(function (menuItem){
+        const menuCategory = menu.filter(function (menuItem) {
             // console.log(menuItem.category)
-            if(category === menuItem.category){
-               return menuItem 
+            if (category === menuItem.category) {
+                return menuItem
             }
-            
+
         })
         // console.log(menuCategory)
-        if(category === "todos"){
+        if (category === "todos") {
             displayMenuItems(menu)
-        }else{
+        } else {
             displayMenuItems(menuCategory)
         }
 
     })
-    
+
 })
 
 
-function displayMenuItems(menuItems){
-    let displayMenu = menuItems.map(function(item){
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
         return `<div class="options" class="japanese">
         <img src=${item.img} alt="">
         <div class="text-content">
             <div class="title">
                 <h2>${item.title}</h2>   
-                <h2 class="price">R$${item.price}</h2> 
+                <h2 class="price">R$${item.price},00</h2> 
             </div>
             <hr>
             <p>${item.desc}
             </p>
         </div>
         </div>`
-    
-        })
-        displayMenu = displayMenu.join("")
-        main.innerHTML = displayMenu;
+
+    })
+    displayMenu = displayMenu.join("")
+    main.innerHTML = displayMenu;
 }
